5b448477f982dee8e278679c145b5843c4a24adc
Tika Llyn <83469447+tikallyn@users.noreply.github.com>
2021-12-16 21:15:25 -0600

    tutorial completed

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+//the uri option specifies the location of our GraphQL server
+  uri: 'http://localhost:4000',
+//cache enables it to store and reuse query results so it doesn't have to make as many network requests, making it quicker
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
---
+++ server/src/schema.js
@@ -0,0 +1,50 @@
+const {gql} = require('apollo-server')
+
+
+const typeDefs = gql`
+type Query {
+  "Get tracks array for homepage grid"
+  tracksForHome: [Track!]!
+}
+"A track is a group of Modules that teaches about a specific topic"
+type Track {
+  id: ID!
+  "The track's title"
+  title: String!
+  "The track's main author"
+  author: Author!
+  "The track's main illustration to display in track card or track page detail"
+  thumbnail: String
+  "The track's approximate length to complete, in minutes"
+  length: Int
+  "The number of modules this track contains"
+  modulesCount: Int
+}
+"Author of a complete Track"
+type Author {
+  id: ID!
+  "Author's first and last name"
+  name: String!
+  "Author's profile picture url"
+  photo: String
+}
+`
+module.exports = typeDefs
+
+// Cats:
+// const typeDefs = gql`
+//   type Query {
+//     spaceCats: [SpaceCat]
+//   }
+//   type SpaceCat {
+//     id: ID!
+//     name: String!
+//     age: Int
+//     missions: [Mission]
+//   }
+//   type Mission {
+//     id: ID!
+//     name: String!
+//     description: String!
+//   }
+// `
